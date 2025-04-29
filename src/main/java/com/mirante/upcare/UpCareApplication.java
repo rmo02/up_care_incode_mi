9c001bd0b2a9e9fba20e13e4606118f1c0df7851
David Gomesh <davidgomesh2016@gmail.com>
2025-04-29 09:54:50 -0300

    Initial commit

---
+++ src/main/java/com/mirante/upcare/UpCareApplication.java
@@ -0,0 +1,13 @@
+package com.mirante.upcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirante/upcare/UpCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirante.upcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
