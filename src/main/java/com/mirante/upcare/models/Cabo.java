41cdffc9ec0b9520dc703c141d49380268c039e0
Rafael Ribeiro <rafaelribeirodzz@gmail.com>
2025-05-13 11:18:08 -0300

    criado entidades(ArCondicionado,Cabo,Combinador,TipoCabo,TipoTorre,Exaustor,Telemetria,Torre)

---
+++ src/main/java/com/mirante/upcare/models/Cabo.java
@@ -0,0 +1,31 @@
+package com.mirante.upcare.models;
+
+import com.mirante.upcare.models.enums.TipoAntena;
+import com.mirante.upcare.models.enums.TipoCabo;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity
+@Getter @Setter
+public class Cabo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "fk_equipamento")
+    private Equipamento equipamento;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TipoCabo tipo;
+
+    @NotNull
+    private Float tamanho;
+
+
+}
---
+++ src/main/java/com/mirante/upcare/models/Combinador.java
@@ -0,0 +1,20 @@
+package com.mirante.upcare.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity
+@Getter @Setter
+public class Combinador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "fk_equipamento")
+    private Equipamento equipamento;
+}
