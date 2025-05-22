afe9be0894c2e690c801518ac946cee0fcb8a63e
DavidGomesh <davidgomesh2016@gmail.com>
2025-05-22 11:56:00 -0300

    Adicionado controllers de equipamento e estacao

---
+++ src/main/java/com/mirante/upcare/controller/EquipamentoController.java
@@ -0,0 +1,29 @@
+package com.mirante.upcare.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mirante.upcare.dto.response.EquipamentoResponse;
+import com.mirante.upcare.mappers.EquipamentoMapper;
+import com.mirante.upcare.repository.EquipamentoRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/equipamentos")
+public class EquipamentoController {
+
+    private final EquipamentoRepository equipamentoRepository;
+
+    @GetMapping
+    public List<EquipamentoResponse> buscarTodos() {
+        return EquipamentoMapper.INSTANCE.toResponseList (
+            equipamentoRepository.findAll()
+        );
+    }
+
+}
---
+++ src/main/java/com/mirante/upcare/controller/EstacaoController.java
@@ -0,0 +1,29 @@
+package com.mirante.upcare.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mirante.upcare.dto.response.EstacaoResponse;
+import com.mirante.upcare.mappers.EstacaoMapper;
+import com.mirante.upcare.repository.EstacaoRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/estacoes")
+public class EstacaoController {
+
+    private final EstacaoRepository estacaoRepository;
+
+    @GetMapping
+    public List<EstacaoResponse> buscarTodos() {
+        return EstacaoMapper.INSTANCE.toResponseList(
+            estacaoRepository.findAll()
+        );
+    }
+    
+}
