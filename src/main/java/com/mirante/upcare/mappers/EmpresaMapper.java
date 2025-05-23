5c0ac129b5d21e0f70692d17a46329ec1272db93
DavidGomesh <davidgomesh2016@gmail.com>
2025-05-23 08:56:39 -0300

    Criado Response e Mapper de Empresa

---
+++ src/main/java/com/mirante/upcare/mappers/EmpresaMapper.java
@@ -0,0 +1,17 @@
+package com.mirante.upcare.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.mirante.upcare.dto.response.EmpresaResponse;
+import com.mirante.upcare.models.Empresa;
+
+@Mapper
+public interface EmpresaMapper {
+    
+    EmpresaMapper INSTANCE = Mappers.getMapper(EmpresaMapper.class);
+
+    @Mapping(source = "id", target = "idEmpresa")
+    EmpresaResponse toResponse(Empresa empresa);
+}
