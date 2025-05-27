144a724494fe8f4dcc8def5191d8421344390c7b
David Gomesh <davidgomesh2016@gmail.com>
2025-05-27 10:56:04 -0300

    Criado Service de Estacao

---
+++ src/main/java/com/mirante/upcare/service/EstacaoService.java
@@ -0,0 +1,26 @@
+package com.mirante.upcare.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mirante.upcare.models.Estacao;
+import com.mirante.upcare.repository.EstacaoRepository;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EstacaoService {
+    
+    private final EstacaoRepository estacaoRepository;
+    
+    public Estacao salvar(@Valid Estacao estacao) {
+        return estacaoRepository.save(estacao);
+    }
+
+    public List<Estacao> buscarTodos() {
+        return estacaoRepository.findAll();
+    }
+}
