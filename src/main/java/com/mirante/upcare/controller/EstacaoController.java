d42331b8cef45567aa9568f0281fd13583a891f0
David Gomesh <davidgomesh2016@gmail.com>
2025-05-27 10:56:15 -0300

    Melhorado Controller de Estacao

--- src/main/java/com/mirante/upcare/controller/EstacaoController.java
+++ src/main/java/com/mirante/upcare/controller/EstacaoController.java
@@ -1,15 +1,24 @@
 package com.mirante.upcare.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mirante.upcare.dto.request.EstacaoRequest;
 import com.mirante.upcare.dto.response.EstacaoResponse;
 import com.mirante.upcare.mappers.EstacaoMapper;
-import com.mirante.upcare.repository.EstacaoRepository;
+import com.mirante.upcare.models.Estacao;
+import com.mirante.upcare.service.EstacaoService;
+import com.mirante.upcare.utils.Pipeline;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -17,12 +26,27 @@ import lombok.AllArgsConstructor;
 @RequestMapping("estacoes")
 public class EstacaoController {
 
-    private final EstacaoRepository estacaoRepository;
+    private final EstacaoService estacaoService;
+
+    @PostMapping
+    public ResponseEntity<UUID> salvar(@RequestBody @Valid EstacaoRequest dto) {
+        return (Pipeline
+            .from(dto)
+            .then(EstacaoMapper.INSTANCE::toEntity)
+            .then(estacaoService::salvar)
+            .then(Estacao::getId)
+            .then(id -> ResponseEntity.status(HttpStatus.CREATED).body(id))
+            .get()
+        );
+    }
 
     @GetMapping
-    public List<EstacaoResponse> buscarTodos() {
-        return EstacaoMapper.INSTANCE.toResponseList(
-            estacaoRepository.findAll()
+    public ResponseEntity<List<EstacaoResponse>> buscarTodos() {
+        return (Pipeline
+            .from(estacaoService.buscarTodos())
+            .then(EstacaoMapper.INSTANCE::toResponseList)
+            .then(ResponseEntity::ok)
+            .get()
         );
     }
     
