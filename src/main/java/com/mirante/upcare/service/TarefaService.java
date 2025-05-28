627486abd568bda6085f7729d1a76167dac0b7e6
elenylsoncbk <elenylsoncm@gmail.com>
2025-05-28 18:33:05 -0300

    Create TarefaService.java

---
+++ src/main/java/com/mirante/upcare/service/TarefaService.java
@@ -0,0 +1,30 @@
+package com.mirante.upcare.service;
+
+import com.mirante.upcare.models.Tarefa;
+import com.mirante.upcare.repository.TarefaRepository;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class TarefaService {
+
+    private final TarefaRepository tarefaRepository;
+
+    public Tarefa salvar(@Valid Tarefa tarefa){
+        return tarefaRepository.save(tarefa);
+    }
+    public List<Tarefa> buscarTodos(){
+        return tarefaRepository.findAll();
+    }
+    public Tarefa buscarPorId(UUID idTarefa){
+        return tarefaRepository.findById(idTarefa).orElseThrow();
+    }
+    public void deletarPorId(UUID idTarefa){
+        tarefaRepository.deleteById(idTarefa);
+    }
+}
