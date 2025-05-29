87605a68f305eacf532c1a46f2b1055e7d957930
elenylsoncbk <elenylsoncm@gmail.com>
2025-05-29 11:29:35 -0300

    Update TarefaEquipamentoMapper.java

--- src/main/java/com/mirante/upcare/mappers/TarefaEquipamentoMapper.java
+++ src/main/java/com/mirante/upcare/mappers/TarefaEquipamentoMapper.java
@@ -1,19 +1,26 @@
 package com.mirante.upcare.mappers;
+import com.mirante.upcare.dto.request.TarefaEquipamentoRequest;
 import com.mirante.upcare.dto.response.TarefaEquipamentoResponse;
 import com.mirante.upcare.models.TarefaEquipamento;
+import com.mirante.upcare.repository.TarefaEquipamentoRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-@Mapper(uses = {EquipamentoMapper.class, TarefaMapper.class})
-public interface TarefaEquipamentoMapper {
 
-    TarefaEquipamentoMapper INSTANCE = Mappers.getMapper(TarefaEquipamentoMapper.class);
+@Mapper(componentModel = "spring", uses = {EquipamentoMapper.class, TarefaMapper.class})
+public abstract class TarefaEquipamentoMapper {
+
+    @Autowired
+    private TarefaEquipamentoRepository tarefaEquipamentoRepository;
+
+    @Mapping(target = "id", ignore = true)
+    abstract public TarefaEquipamento toEntity(TarefaEquipamentoRequest tarefaEquipamentoRequest);
 
     @Mapping(source = "id", target = "idTarefaEquipamento")
-    TarefaEquipamentoResponse toResponse(TarefaEquipamento tarefaEquipamento);
+    abstract public TarefaEquipamentoResponse toResponse(TarefaEquipamento tarefaEquipamento);
 
-    List<TarefaEquipamentoResponse> toResponseList(List<TarefaEquipamento> tarefaEquipamentos);
+    abstract public List<TarefaEquipamentoResponse> toResponseList(List<TarefaEquipamento> tarefaEquipamentos);
 
 }
