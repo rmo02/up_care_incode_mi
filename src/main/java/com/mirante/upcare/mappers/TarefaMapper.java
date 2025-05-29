d882a31cd8cadf5debd9d9c7d3a049a699d56a35
elenylsoncbk <elenylsoncm@gmail.com>
2025-05-29 11:29:38 -0300

    Update TarefaMapper.java

--- src/main/java/com/mirante/upcare/mappers/TarefaMapper.java
+++ src/main/java/com/mirante/upcare/mappers/TarefaMapper.java
@@ -1,22 +1,29 @@
 package com.mirante.upcare.mappers;
 
+import com.mirante.upcare.dto.request.TarefaRequest;
 import com.mirante.upcare.dto.response.TarefaResponse;
 import com.mirante.upcare.models.Tarefa;
+import com.mirante.upcare.repository.TarefaRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Mapper(uses = ManutencaoMapper.class)
-public interface TarefaMapper {
+@Mapper(componentModel = "spring", uses = ManutencaoMapper.class)
+public abstract class TarefaMapper {
 
-    TarefaMapper INSTANCE = Mappers.getMapper(TarefaMapper.class);
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @Mapping(target = "id", ignore = true)
+    abstract public Tarefa toEntity(TarefaRequest tarefaRequest);
+
 
     @Mapping(source = "id", target = "idTarefa")
-    TarefaResponse toResponse(Tarefa tarefa);
+    abstract public TarefaResponse toResponse(Tarefa tarefa);
 
-    List<TarefaResponse> toResponseList(List<Tarefa> tarefas);
+    abstract public List<TarefaResponse> toResponseList(List<Tarefa> tarefas);
 
 
 
