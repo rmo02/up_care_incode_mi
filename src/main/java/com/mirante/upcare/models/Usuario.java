5b41228826ac44faf54cf33497a00f3714a1f2ba
elenylsoncbk <elenylsoncm@gmail.com>
2025-05-29 11:29:52 -0300

    Update Usuario.java

--- src/main/java/com/mirante/upcare/models/Usuario.java
+++ src/main/java/com/mirante/upcare/models/Usuario.java
@@ -2,12 +2,7 @@ package com.mirante.upcare.models;
 
 import java.util.UUID;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
