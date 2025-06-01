ed0b6b47f7de7a38c79c6c0919e49a07b6a33534
elenylsoncbk <elenylsoncm@gmail.com>
2025-06-01 15:49:26 -0300

    Update TarefaEquipamentoController.java

--- src/main/java/com/mirante/upcare/controller/TarefaEquipamentoController.java
+++ src/main/java/com/mirante/upcare/controller/TarefaEquipamentoController.java
@@ -1,25 +1,65 @@
 package com.mirante.upcare.controller;
+import com.mirante.upcare.dto.request.TarefaEquipamentoRequest;
 import com.mirante.upcare.dto.response.TarefaEquipamentoResponse;
 import com.mirante.upcare.mappers.TarefaEquipamentoMapper;
-import com.mirante.upcare.repository.TarefaEquipamentoRepository;
+import com.mirante.upcare.models.TarefaEquipamento;
+import com.mirante.upcare.service.TarefaEquipamentoService;
+import com.mirante.upcare.utils.Pipeline;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("TarefaEquipamento")
 public class TarefaEquipamentoController {
 
-    private final TarefaEquipamentoRepository tarefaEquipamentoRepository;
+    private TarefaEquipamentoService tarefaEquipamentoService;
+    private TarefaEquipamentoMapper tarefaEquipamentoMapper;
 
-    @GetMapping
-    public List<TarefaEquipamentoResponse> buscarTodos(){
-        return TarefaEquipamentoMapper.INSTANCE.toResponseList(
-                tarefaEquipamentoRepository.findAll()
+
+    @PostMapping
+    public ResponseEntity<UUID> salvar(@RequestBody @Valid TarefaEquipamentoRequest dto){
+        return (Pipeline
+                .from(dto)
+                .then(tarefaEquipamentoMapper::toEntity)
+                .then(tarefaEquipamentoService::salvar)
+                .then(TarefaEquipamento::getId)
+                .then(id-> ResponseEntity.status(HttpStatus.CREATED).body(id))
+                .get()
         );
     }
+    @GetMapping
+    public ResponseEntity<List<TarefaEquipamentoResponse>> buscarTodos(){
+        return (Pipeline
+                .from(tarefaEquipamentoService.buscarTodos())
+                .then(tarefaEquipamentoMapper::toResponseList)
+                .then(ResponseEntity::ok)
+                .get()
+        );
+    }
+
+    @GetMapping("{idtarefaEquipamento}")
+    public ResponseEntity<TarefaEquipamentoResponse> buscarPorId(@PathVariable UUID idtarefaEquipamento) {
+        return (Pipeline
+                .from(idtarefaEquipamento)
+                .then(tarefaEquipamentoService::buscarPorId)
+                .then(tarefaEquipamentoMapper::toResponse)
+                .then(ResponseEntity::ok)
+                .get()
+        );
+    }
+    @DeleteMapping("{idtarefaEquipamento}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable UUID idtarefaEquipamento) {
+        tarefaEquipamentoService.deletarPorId(idtarefaEquipamento);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
