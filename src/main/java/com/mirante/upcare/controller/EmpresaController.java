9f7d1f73970fe3aed5d51445b41a64a3a996e6ff
elenylsoncbk <elenylsoncm@gmail.com>
2025-06-01 15:49:15 -0300

    Update EmpresaController.java

--- src/main/java/com/mirante/upcare/controller/EmpresaController.java
+++ src/main/java/com/mirante/upcare/controller/EmpresaController.java
@@ -1,25 +1,47 @@
 package com.mirante.upcare.controller;
+import com.mirante.upcare.dto.request.EmpresaRequest;
 import com.mirante.upcare.dto.response.EmpresaResponse;
 import com.mirante.upcare.mappers.EmpresaMapper;
-import com.mirante.upcare.repository.EmpresaRepository;
+import com.mirante.upcare.models.Empresa;
+import com.mirante.upcare.service.EmpresaService;
+import com.mirante.upcare.utils.Pipeline;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("Empresa")
 public class EmpresaController {
 
-    private final EmpresaRepository empresaRepository;
+    private EmpresaService empresaService;
+    private EmpresaMapper empresaMapper;
 
-    @GetMapping
-    public List<EmpresaResponse> buscarTodos() {
-        return EmpresaMapper.INSTANCE.toResponseList (
-                empresaRepository.findAll()
+    @PostMapping
+    public ResponseEntity<UUID> salvar(@RequestBody @Valid EmpresaRequest dto){
+        return (Pipeline
+                .from(dto)
+                .then(empresaMapper::toEntity)
+                .then(empresaService::salvar)
+                .then(Empresa::getId)
+                .then(id-> ResponseEntity.status(HttpStatus.CREATED).body(id))
+                .get()
+        );
+    }
+    public ResponseEntity<List<EmpresaResponse>> buscarTodos(){
+        return (Pipeline
+                .from(empresaService.buscarTodos())
+                .then(empresaMapper::toResponseList)
+                .then(ResponseEntity::ok)
+                .get()
         );
     }
 
