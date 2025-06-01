f99d6a3b6a7bba12f75fd0a48300ad09064c7bfb
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-06-01 18:41:12 -0300

    Limpando imports

--- src/main/java/com/mirante/upcare/dto/request/QuadroRequest.java
+++ src/main/java/com/mirante/upcare/dto/request/QuadroRequest.java
@@ -2,8 +2,6 @@ package com.mirante.upcare.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.util.UUID;
-
 public record QuadroRequest(
         @NotNull
         EquipamentoRequest equipamento
