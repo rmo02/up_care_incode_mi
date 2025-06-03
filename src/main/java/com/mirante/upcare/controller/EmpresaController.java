3af51c5f93b7e60a6677f970ea4dfe90ec0b520c
elenylsoncbk <elenylsoncm@gmail.com>
2025-06-03 10:29:54 -0300

    Atualizado Controller, Mapper e Service de Empresa

--- src/main/java/com/mirante/upcare/controller/EmpresaController.java
+++ src/main/java/com/mirante/upcare/controller/EmpresaController.java
@@ -1,48 +1,83 @@
 package com.mirante.upcare.controller;
-import com.mirante.upcare.dto.request.EmpresaRequest;
-import com.mirante.upcare.dto.response.EmpresaResponse;
-import com.mirante.upcare.mappers.EmpresaMapper;
-import com.mirante.upcare.models.Empresa;
-import com.mirante.upcare.service.EmpresaService;
-import com.mirante.upcare.utils.Pipeline;
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import com.mirante.upcare.dto.request.EmpresaRequest;
+import com.mirante.upcare.dto.response.EmpresaResponse;
+import com.mirante.upcare.mappers.EmpresaMapper;
+import com.mirante.upcare.service.EmpresaService;
+import com.mirante.upcare.utils.Pipeline;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("Empresa")
 public class EmpresaController {
 
-    private EmpresaService empresaService;
-    private EmpresaMapper empresaMapper;
+    private final EmpresaService empresaService;
+    private final EmpresaMapper empresaMapper;
 
     @PostMapping
-    public ResponseEntity<UUID> salvar(@RequestBody @Valid EmpresaRequest dto){
+    public ResponseEntity<Object> salvar(@RequestBody @Valid EmpresaRequest dto){
         return (Pipeline
-                .from(dto)
-                .then(empresaMapper::toEntity)
-                .then(empresaService::salvar)
-                .then(Empresa::getId)
-                .then(id-> ResponseEntity.status(HttpStatus.CREATED).body(id))
-                .get()
+            .from(dto)
+            .then(empresaMapper::toEntity)
+            .then(empresaService::salvar)
+            .then(e -> ResponseEntity.status(HttpStatus.CREATED).build())
+            .get()
         );
     }
+
+    @GetMapping
     public ResponseEntity<List<EmpresaResponse>> buscarTodos(){
         return (Pipeline
-                .from(empresaService.buscarTodos())
-                .then(empresaMapper::toResponseList)
-                .then(ResponseEntity::ok)
-                .get()
+            .from(empresaService.buscarTodos())
+            .then(empresaMapper::toResponseList)
+            .then(ResponseEntity::ok)
+            .get()
         );
+    }
+
+    @GetMapping("idEmpresa")
+    public ResponseEntity<EmpresaResponse> buscarPorId(@PathVariable UUID idEmpresa) {
+        return (Pipeline
+            .from(idEmpresa)
+            .then(empresaService::buscarPorId)
+            .then(empresaMapper::toResponse)
+            .then(ResponseEntity::ok)
+            .get()
+        );
+    }
+
+    @PutMapping("{idEmpresa}")
+    public ResponseEntity<Object> atualizarPorId(@PathVariable UUID idEmpresa, @Valid @RequestBody EmpresaRequest dto) {
+        return (Pipeline
+            .from(dto)
+            .then(empresaMapper::toEntity)
+            .then(empresaAtualizada -> empresaService.atualiarPorId(idEmpresa, empresaAtualizada))
+            .then(e -> ResponseEntity.noContent().build())
+            .get()
+        );
+    }
+
+    @DeleteMapping("{idEmpresa}")
+    public ResponseEntity<Object> deletarPorId(@PathVariable UUID idEmpresa) {
+        empresaService.deletarPorId(idEmpresa);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/mirante/upcare/service/EmpresaService.java
+++ src/main/java/com/mirante/upcare/service/EmpresaService.java
@@ -4,9 +4,12 @@ import com.mirante.upcare.models.Empresa;
 import com.mirante.upcare.repository.EmpresaRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,8 +20,23 @@ public class EmpresaService {
     public Empresa salvar(@Valid Empresa empresa){
         return empresaRepository.save(empresa);
     }
+
     public List<Empresa> buscarTodos() {
         return empresaRepository.findAll();
+    }
+
+    public Empresa buscarPorId(UUID idEmpresa) {
+        return empresaRepository.findById(idEmpresa).orElseThrow();
+    }
+
+    public Empresa atualiarPorId(UUID idEmpresa, Empresa empresaAtualizada) {
+        Empresa empresaExistente = buscarPorId(idEmpresa);
+        BeanUtils.copyProperties(empresaAtualizada, empresaExistente, "id");
+        return salvar(empresaExistente);
+    }
+
+    public void deletarPorId(UUID idEmpresa) {
+        empresaRepository.deleteById(idEmpresa);
     }
 
 }
